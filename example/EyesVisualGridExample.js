73454c05f910f7584f7a3fc6a007ca2216ff0c59
yurii <yurii.besedin@applitools.com>
2019-06-04 06:33:54 +0300

    Add batchSequenceName

--- example/EyesVisualGridExample.js
+++ example/EyesVisualGridExample.js
@@ -2,7 +2,17 @@
 
 const chromedriver = require('chromedriver');
 const {remote} = require('webdriverio');
-const {By, Eyes, Target, VisualGridRunner, Configuration, BrowserType, DeviceName, ScreenOrientation} = require('../index'); // should be replaced to '@applitools/eyes-webdriverio'
+const {
+  By,
+  Eyes,
+  Target,
+  VisualGridRunner,
+  Configuration,
+  BrowserType,
+  DeviceName,
+  ScreenOrientation,
+  BatchInfo
+} = require('../index'); // should be replaced to '@applitools/eyes-webdriverio'
 
 (async () => {
   chromedriver.start();
@@ -19,7 +29,11 @@ const {By, Eyes, Target, VisualGridRunner, Configuration, BrowserType, DeviceNam
   const eyes = new Eyes(new VisualGridRunner(3));
 
   try {
+    const batchInfo = new BatchInfo();
+    batchInfo.setSequenceName('alpha sequence');
+
     const configuration = new Configuration();
+    configuration.setBatch(batchInfo);
     configuration.setAppName('Eyes Examples');
     configuration.setTestName('My first Javascript test!');
     configuration.addBrowser(800, 600, BrowserType.CHROME);
