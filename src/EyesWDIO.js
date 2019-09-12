d760e7605bd5d3fe95223e0edbe11a5a0a215277
yurii <yurii.besedin@applitools.com>
2019-09-12 15:28:11 +0300

    feature: add `abortAsync` method

--- src/EyesWDIO.js
+++ src/EyesWDIO.js
@@ -843,6 +843,12 @@ class EyesWDIO extends EyesBase {
     await this.close(false);
   }
 
+  /**
+   * @return {Promise}
+   */
+  async abortAsync() {
+    await this.abort();
+  }
 
   /**
    * @param {boolean} [throwEx]
