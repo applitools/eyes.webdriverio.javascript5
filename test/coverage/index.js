af6c725a14b52d8cc90352c7c90984f505a994b5
David Haeffner <dhaeffner@gmail.com>
2019-12-22 13:35:52 +0200

    update coverage test implementation to use latest API and opt into the additional tests

--- test/coverage/index.js
+++ test/coverage/index.js
@@ -1,82 +1,68 @@
 const chromedriver = require('chromedriver');
 const {remote} = require('webdriverio');
 const {By} = require('selenium-webdriver');
-const {makeRun, makeResourcePool, findResourceInPool} = require('@applitools/sdk-test-kit')
+const {makeRunTests} = require('@applitools/sdk-test-kit')
 const {Eyes, BatchInfo, RectangleSize, StitchMode, VisualGridRunner, Target} = require('../../index')
 
 const sdkName = 'eyes.webdriverio.javascript5'
 const batch = new BatchInfo(`JS Coverage Tests - ${sdkName}`)
 
-function initialize(context) {
+async function initialize({displayName, executionMode}) {
   let eyes
   let driver
 
-  async function beforeAll() {
-    const resourcePool = await makeResourcePool(10, async () => {
-      const driver = await remote({
-        logLevel: 'error',
-        capabilities: {
-          browserName: 'chrome',
-          'goog:chromeOptions': {
-            args: [
-              '--headless',
-            ]
-          }
-        }
-      })
-      return { driver, isAvailable: true }
-    })
-    return { resourcePool }
-  }
-
-  async function afterAll(sharedContext) {
-    for (const index in sharedContext.resourcePool) {
-      await sharedContext.resourcePool[index].driver.deleteSession()
+  driver = await remote({
+    logLevel: 'error',
+    capabilities: {
+      browserName: 'chrome',
+      'goog:chromeOptions': {
+        args: [
+          '--headless',
+        ]
+      }
     }
-  }
-
-  async function beforeEach() {
-    resource = await findResourceInPool(context.executionMode.resourcePool, resource => {
-      return resource.isAvailable
-    })
-    driver = resource.driver
-    resource.isAvailable = false
-    if (context.executionMode.isVisualGrid) {
-      eyes = new Eyes(new VisualGridRunner())
-    } else if (context.executionMode.isCssStitching) {
-      eyes = new Eyes()
-      eyes.setStitchMode(StitchMode.CSS)
-    } else if (context.executionMode.isScrollStitching) {
-      eyes = new Eyes()
-      eyes.setStitchMode(StitchMode.SCROLL)
-    }
-    eyes.setBatch(batch)
-  }
-
-  function afterEach() {
-    resource.isAvailable = true
-  }
+  })
+  eyes = executionMode.isVisualGrid ? new Eyes(new VisualGridRunner()) : new Eyes()
+  executionMode.isCssStitching ? eyes.setStitchMode(StitchMode.CSS) : undefined
+  executionMode.isScrollStitching ? eyes.setStitchMode(StitchMode.SCROLL) : undefined
+  eyes.setBatch(batch)
 
   async function visit(url) {
     await driver.url(url)
   }
 
   async function open(options) {
-    await eyes.open(
+    driver = await eyes.open(
       driver,
       sdkName,
-      context.displayName,
+      displayName,
       RectangleSize.parse(options.viewportSize),
     )
   }
 
-  async function check(options = {}) {
-    if (options.isClassicApi) {
-      options.locator ? await eyes.checkRegion(By.css(options.locator)) : await eyes.checkWindow()
+  async function checkFrame({isClassicApi = false, locator, tag, matchTimeout} = {}) {
+    if (isClassicApi) {
+      await eyes.checkFrame(By.css(locator), matchTimeout, tag)
     } else {
-      options.locator
-        ? await eyes.check(undefined, Target.region(By.css(options.locator)).fully())
-        : await eyes.check(undefined, Target.window().fully())
+      await eyes.check(tag, Target.frame(By.css(locator)).fully())
+    }
+  }
+
+  async function checkRegion({isClassicApi = false, locator, tag, matchTimeout} = {}) {
+    if (isClassicApi) {
+      await eyes.checkRegion(By.css(locator), matchTimeout, tag)
+    } else {
+      await eyes.check(tag, Target.region(By.css(locator)).fully())
+    }
+  }
+
+  async function checkWindow({isClassicApi = false, isFully = false, tag, matchTimeout} = {}) {
+    if (isClassicApi) {
+      await eyes.checkWindow(tag, matchTimeout, isFully)
+    } else {
+      isFully
+        ? await eyes.check(undefined, Target.window().fully())
+        : await eyes.check(undefined, Target.window())
     }
   }
 
@@ -84,27 +70,49 @@ function initialize(context) {
     await eyes.close(options)
   }
 
-  return {beforeAll, afterAll, beforeEach, afterEach, visit, open, check, close}
+  async function cleanup() {
+    await driver.deleteSession()
+  }
+
+  return {visit, open, checkFrame, checkRegion, checkWindow, close, cleanup}
 }
 
 const supportedTests = [
+  {name: 'checkFrameClassic', executionMode: {isVisualGrid: true}},
+  {name: 'checkFrameClassic', executionMode: {isCssStitching: true}},
+  {name: 'checkFrameClassic', executionMode: {isScrollStitching: true}},
   {name: 'checkRegionClassic', executionMode: {isVisualGrid: true}},
   {name: 'checkRegionClassic', executionMode: {isCssStitching: true}},
   {name: 'checkRegionClassic', executionMode: {isScrollStitching: true}},
+  {name: 'checkRegionClassicWithOverFlow', executionMode: {isVisualGrid: true}},
+  {name: 'checkRegionClassicWithOverFlow', executionMode: {isCssStitching: true}},
+  {name: 'checkRegionClassicWithOverFlow', executionMode: {isScrollStitching: true}},
+  {name: 'checkWindowClassicViewport', executionMode: {isVisualGrid: true}},
+  {name: 'checkWindowClassicViewport', executionMode: {isCssStitching: true}},
+  {name: 'checkWindowClassicViewport', executionMode: {isScrollStitching: true}},
+  {name: 'checkWindowClassicFully', executionMode: {isVisualGrid: true}},
+  {name: 'checkWindowClassicFully', executionMode: {isCssStitching: true}},
+  {name: 'checkWindowClassicFully', executionMode: {isScrollStitching: true}},
+  {name: 'checkFrameFluent', executionMode: {isVisualGrid: true}},
+  {name: 'checkFrameFluent', executionMode: {isCssStitching: true}},
+  {name: 'checkFrameFluent', executionMode: {isScrollStitching: true}},
   {name: 'checkRegionFluent', executionMode: {isVisualGrid: true}},
   {name: 'checkRegionFluent', executionMode: {isCssStitching: true}},
   {name: 'checkRegionFluent', executionMode: {isScrollStitching: true}},
-  {name: 'checkWindowClassic', executionMode: {isVisualGrid: true}},
-  {name: 'checkWindowClassic', executionMode: {isCssStitching: true}},
-  {name: 'checkWindowClassic', executionMode: {isScrollStitching: true}},
-  {name: 'checkWindowFluent', executionMode: {isVisualGrid: true}},
-  {name: 'checkWindowFluent', executionMode: {isCssStitching: true}},
-  {name: 'checkWindowFluent', executionMode: {isScrollStitching: true}},
+  {name: 'checkRegionFluentWithOverFlow', executionMode: {isVisualGrid: true}},
+  {name: 'checkRegionFluentWithOverFlow', executionMode: {isCssStitching: true}},
+  {name: 'checkRegionFluentWithOverFlow', executionMode: {isScrollStitching: true}},
+  {name: 'checkWindowFluentViewport', executionMode: {isVisualGrid: true}},
+  {name: 'checkWindowFluentViewport', executionMode: {isCssStitching: true}},
+  {name: 'checkWindowFluentViewport', executionMode: {isScrollStitching: true}},
+  {name: 'checkWindowFluentFully', executionMode: {isVisualGrid: true}},
+  {name: 'checkWindowFluentFully', executionMode: {isCssStitching: true}},
+  {name: 'checkWindowFluentFully', executionMode: {isScrollStitching: true}},
 ]
 
 const returnPromise = true
 chromedriver.start(['--port=4444', '--url-base=wd/hub', '--silent'], returnPromise).then(() => {
-  makeRun(sdkName, initialize).run(supportedTests).then(() => {
+  makeRunTests(sdkName, initialize).runTests(supportedTests).then(() => {
     chromedriver.stop()
   })
 }).catch(console.error)
